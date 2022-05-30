aedc4dc0d351ae13d5f72cd2fb123a0fef574327
DIALOGGROUP\4440 <simon.kaufmann@accesshq.com>
2022-05-30 10:28:53 +1000

    added new test

--- src/test/java/com/accesshq/apitests/ApiTestSuite.java
+++ src/test/java/com/accesshq/apitests/ApiTestSuite.java
@@ -6,14 +6,18 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class ApiTestSuite {
 
+
     @Test
     public void TestStatus() {
-        given().
-                param("q", "Tascott").and().header("auth-key", "c912db24-d4be-4303-97ff-92f940a58b8b").
-        when().
-                get("https://digitalapi.auspost.com.au/postcode/search.json").
-        then().
-                assertThat().statusCode(200);
+
+        given().param("q", "Tascott").and().header("auth-key", "c912db24-d4be-4303-97ff-92f940a58b8b").
+        when().get("https://digitalapi.auspost.com.au/postcode/search.json").
+        then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void TestNewTest() {
+        // this will one day be a test
     }
 
     @Test
